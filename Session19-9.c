c2a52fae01a3eb563e4dd87572a23bf726fe62cc
latp1102 <latp1102@gmail.com>
2024-12-21 22:46:28 +0700

    Session19 - 09

---
+++ Session19-9.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <string.h>
+
+struct SinhVien {
+	int id;
+	char name[50];
+	int age;
+	char phoneNumber[15];
+};
+
+void inSV(struct SinhVien menu[], int n);
+void themSV(struct SinhVien menu[], int *n);
+void suaSV(struct SinhVien menu[], int n);
+void xoaSV(struct SinhVien menu[], int *n);
+void timKiemSV(struct SinhVien menu[], int n);
+void sapXepSV(struct SinhVien menu[], int n);
+
+int main(){
+	struct SinhVien menu[100] = {
+        { 1, "nguyen van a", 30, "0111111111" },	
+        { 2, "nguyen van d", 40, "0222222222" },	
+        { 3, "nguyen van e", 35, "0333333333" },
+        { 4, "nguyen van c", 60, "0444444444" },
+        { 5, "nguyen van b", 55, "0555555555" },
+    };
+    
+    int chose;
+	int n = 5;
+    do {
+    	printf("--------Menu--------\n");
+    	printf("1. Hien thi danh sach sinh vien\n ");
+    	printf("2. Them sinh vien\n ");
+    	printf("3. Sua thong tin sinh vien\n ");
+    	printf("4. Xoa sinh vien\n ");
+    	printf("5. Tim kiem sinh vien\n ");
+    	printf("6. Sap xep danh sach sinh vien\n ");
+    	printf("7. Thoat\n ");
+    	printf("Lua chon cua ban: ");
+    	scanf("%d",&chose);
+    	fflush(stdin);
+    	switch( chose){
+    		case 1:{
+    			inSV(menu, n);
+				break;
+			}
+			case 2:{
+				themSV(menu, &n);
+				break;
+			}
+			case 3:{
+				suaSV(menu, n);
+				break;
+			}
+			case 4:{
+				xoaSV(menu, &n);
+				break;
+			}
+			case 5:{
+				timKiemSV(menu, n);
+				break;
+			}
+			case 6:{
+				sapXepSV(menu, n); 
+				break;
+			}
+			case 7:{
+				printf("\nBan chon thoat");
+				break;
+			}
+		}
+	}while( chose != 7 );
+
+	return 0;
+}
+
+void inSV( struct SinhVien sv[], int n){
+	printf("\nPhan tu trong mang: ");
+	for ( int i = 0; i< n; i++){
+		printf("\n\nID: %d", sv[i].id);
+		printf("\nTen: %s", sv[i].name);
+		printf("\nTuoi: %d", sv[i].age);
+		printf("\nSdt: %s ", sv[i].phoneNumber);
+	}	
+}
+
+void themSV( struct SinhVien sv[], int *n){
+	int index;
+	printf("\nVi tri them(1 - %d): ", *n+1);
+	scanf("%d", &index);
+	
+	while ( index < 1 || index > (*n)+1 ){
+		printf("\nVui long nhap dung!");
+		printf("\nVi tri them(1 - %d): ", (*n)+1);
+		scanf("%d", &index);
+	}
+	fflush(stdin);
+	for ( int i = *n; i>= index ; i--){
+		sv[i].id = sv[i-1].id +1;
+		strcpy( sv[i].name, sv[i-1].name);
+		sv[i].age = sv[i-1].age;
+		strcpy( sv[i].phoneNumber, sv[i-1].phoneNumber);
+	}
+	
+	sv[index-1].id = index ;
+	printf("\nNhap ten sv: ");
+	fgets( sv[index-1].name, sizeof( sv[index-1].name), stdin);
+	int size = strlen(sv[index-1].name);
+	sv[index-1].name[size-1] = '\0';
+	
+	printf("Nhap tuoi: ");
+	scanf("%f", &sv[index-1].age);
+	fflush(stdin);
+	
+	printf("\nNhap sdt: ");
+	fgets( sv[index-1].phoneNumber, sizeof( sv[index-1].phoneNumber), stdin);
+	
+	(*n)++;
+	printf("\nKet qua: \n");
+	for ( int i = 0; i< *n ; i++){
+		printf("\n\nID: %d", sv[i].id);
+		printf("\nTen: %s", sv[i].name);
+		printf("\nTuoi: %d", sv[i].age);
+		printf("\nSdt: %s ", sv[i].phoneNumber);
+	}
+}
+
+void suaSV( struct SinhVien sv[], int n){
+	int index;
+	printf("\nVi tri sua(1 - %d): ", n);
+	scanf("%d", &index);
+	
+	while ( index < 1 || index > n ){
+		printf("\nVui long nhap dung!");
+		printf("\nVi tri sua(1 - %d): ", n);
+		scanf("%d", &index);
+	}
+	fflush(stdin);
+	sv[index-1].id = index ;
+	printf("\nNhap ten sv: ");
+	fgets( sv[index-1].name, sizeof( sv[index-1].name), stdin);
+	int len = strlen(sv[index-1].name);
+	sv[index-1].name[len-1] = '\0';
+	
+	printf("Nhap tuoi: ");
+	scanf("%f", &sv[index-1].age);
+	fflush(stdin);
+	
+	printf("\nNhap sdt: ");
+	fgets( sv[index-1].phoneNumber, sizeof( sv[index-1].phoneNumber), stdin);
+	
+	printf("\nKet qua: \n");
+	for ( int i = 0; i< n; i++){
+		printf("\n\nID: %d", sv[i].id);
+		printf("\nTen: %s", sv[i].name);
+		printf("\nTuoi: %d", sv[i].age);
+		printf("\nSdt: %s ", sv[i].phoneNumber);
+	}
+}
+
+void xoaSV( struct SinhVien sv[], int *n){
+	int index;
+	printf("\nVi tri xoa(1 - %d): ", *n);
+	scanf("%d", &index);
+	
+	while( index < 1 || index > *n ){
+		printf("\nVui long nhap dung!");
+		printf("\nVi tri them(1 - %d): ", *n );
+		scanf("%d", &index);
+	}
+	fflush(stdin);
+	for( int i = index-1 ; i < (*n)-1  ; i++){
+		sv[i].id = sv[i+1].id ;
+		strcpy( sv[i].name, sv[i+1].name);
+		sv[i].age = sv[i+1].age;
+		strcpy( sv[i].phoneNumber, sv[i+1].phoneNumber);
+	}
+	
+	(*n)--;
+	printf("\nKet qua: \n");
+	for ( int i = 0; i< *n ; i++){
+		printf("\n\nID: %d", sv[i].id);
+		printf("\nTen: %s", sv[i].name);
+		printf("\nTuoi: %d", sv[i].age);
+		printf("\nSdt: %s ", sv[i].phoneNumber);
+	}
+}
+
+void sapXepSV( struct SinhVien sv[], int n){
+	for( int i = 0; i< n; i++){
+		for ( int j = 0; j< n-1-i; j++){
+			if ( strcmp(sv[j].name, sv[j+1].name ) > 0 ){
+				struct SinhVien temp = sv[j];
+                sv[j] = sv[j+1];
+                sv[j+1] = temp;
+			}
+		}
+	}
+	printf("\nKet qua: \n");
+	for ( int i = 0; i< n ; i++){
+		printf("\n\nID: %d", sv[i].id);
+		printf("\nTen: %s", sv[i].name);
+		printf("\nTuoi: %d", sv[i].age);
+		printf("\nSdt: %s ", sv[i].phoneNumber);
+	}
+}
+
+void timKiemSV( struct SinhVien sv[], int n){
+	char input[50];
+	int count = 0;
+	
+	printf("\nNhap ten can tim: ");
+	fgets( input, 50, stdin);
+	int size = strlen(input);
+	input[size-1] = '\0';
+	
+	for ( int i = 0; i< n; i++){
+		if ( strcmp(sv[i].name, input) == 0 ){
+			count = 1;
+			printf("\nSV can tim: ");
+			printf("\n\nID: %d", sv[i].id);
+			printf("\nTen: %s", sv[i].name);
+			printf("\nTuoi: %d", sv[i].age);
+			printf("\nSdt: %s ", sv[i].phoneNumber);
+			break;
+		}
+	}
+	if ( count == 0 ) printf("\n Khong tim thay sv!");
+}
+
